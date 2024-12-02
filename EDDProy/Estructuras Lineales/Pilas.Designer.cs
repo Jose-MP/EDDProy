029c82d61044f3edf6cf4b79b2ad5e4432bd455c
Jose-MP <al23760328@ite.edu.mx>
2024-12-01 18:59:22 -0800

    pilasprueba

---
+++ EDDProy/Estructuras Lineales/Pilas.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace ExamenUnidad2
+{
+    partial class Pilas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pilas));
+            this.BtnRegresar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtNodo = new System.Windows.Forms.TextBox();
+            this.BtnIngresar = new System.Windows.Forms.Button();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.ComboPosi = new System.Windows.Forms.ComboBox();
+            this.lstPila = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnMostrar = new System.Windows.Forms.Button();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnRegresar
+            // 
+            this.BtnRegresar.Location = new System.Drawing.Point(12, 410);
+            this.BtnRegresar.Name = "BtnRegresar";
+            this.BtnRegresar.Size = new System.Drawing.Size(88, 28);
+            this.BtnRegresar.TabIndex = 0;
+            this.BtnRegresar.Text = "Regresar";
+            this.BtnRegresar.UseVisualStyleBackColor = true;
+            this.BtnRegresar.Click += new System.EventHandler(this.BtnRegresar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingresar nodo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Posicion (opcional):";
+            // 
+            // TxtNodo
+            // 
+            this.TxtNodo.Location = new System.Drawing.Point(114, 41);
+            this.TxtNodo.Name = "TxtNodo";
+            this.TxtNodo.Size = new System.Drawing.Size(185, 20);
+            this.TxtNodo.TabIndex = 3;
+            // 
+            // BtnIngresar
+            // 
+            this.BtnIngresar.Location = new System.Drawing.Point(62, 349);
+            this.BtnIngresar.Name = "BtnIngresar";
+            this.BtnIngresar.Size = new System.Drawing.Size(124, 29);
+            this.BtnIngresar.TabIndex = 5;
+            this.BtnIngresar.Text = "Push";
+            this.BtnIngresar.UseVisualStyleBackColor = true;
+            this.BtnIngresar.Click += new System.EventHandler(this.BtnIngresar_Click);
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(216, 349);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(124, 29);
+            this.BtnEliminar.TabIndex = 6;
+            this.BtnEliminar.Text = "Pop";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // ComboPosi
+            // 
+            this.ComboPosi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboPosi.FormattingEnabled = true;
+            this.ComboPosi.Items.AddRange(new object[] {
+            "Inicio",
+            "Final"});
+            this.ComboPosi.Location = new System.Drawing.Point(138, 77);
+            this.ComboPosi.Name = "ComboPosi";
+            this.ComboPosi.Size = new System.Drawing.Size(117, 21);
+            this.ComboPosi.TabIndex = 8;
+            this.ComboPosi.SelectedIndexChanged += new System.EventHandler(this.ComboPosi_SelectedIndexChanged);
+            // 
+            // lstPila
+            // 
+            this.lstPila.FormattingEnabled = true;
+            this.lstPila.Location = new System.Drawing.Point(337, 41);
+            this.lstPila.Name = "lstPila";
+            this.lstPila.Size = new System.Drawing.Size(293, 290);
+            this.lstPila.TabIndex = 27;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(51, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 168);
+            this.button1.TabIndex = 28;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // BtnMostrar
+            // 
+            this.BtnMostrar.Location = new System.Drawing.Point(363, 349);
+            this.BtnMostrar.Name = "BtnMostrar";
+            this.BtnMostrar.Size = new System.Drawing.Size(124, 29);
+            this.BtnMostrar.TabIndex = 29;
+            this.BtnMostrar.Text = "Mostrar";
+            this.BtnMostrar.UseVisualStyleBackColor = true;
+            this.BtnMostrar.Click += new System.EventHandler(this.BtnMostrar_Click);
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(506, 350);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(124, 28);
+            this.BtnBuscar.TabIndex = 30;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // Pilas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 450);
+            this.Controls.Add(this.BtnBuscar);
+            this.Controls.Add(this.BtnMostrar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstPila);
+            this.Controls.Add(this.ComboPosi);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.BtnIngresar);
+            this.Controls.Add(this.TxtNodo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnRegresar);
+            this.Name = "Pilas";
+            this.Text = "Pilas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnRegresar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtNodo;
+        private System.Windows.Forms.Button BtnIngresar;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.ComboBox ComboPosi;
+        private System.Windows.Forms.ListBox lstPila;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnMostrar;
+        private System.Windows.Forms.Button BtnBuscar;
+    }
+}
---
+++ EDDProy/Estructuras Lineales/Pilas.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenUnidad2
+{
+    public partial class Pilas : Form
+    {
+
+        Pila miPila = new Pila();
+        public class Nodo
+        {
+            public string Dato { get; set; }
+            public Nodo Sig { get; set; }
+
+            public Nodo(string dato)
+            {
+                Dato = dato;
+                Sig = null;
+            }
+        }
+        public class Pila
+        {
+            private Nodo top;
+            public int Count { get; private set; }
+
+            public Pila()
+            {
+                top = null;
+                Count = 0;
+            }
+            public void PushInicio(string valor)
+            {
+                Nodo nuevoNodo = new Nodo(valor);
+                nuevoNodo.Sig = top;
+                top = nuevoNodo;
+                Count++;
+            }
+            public string PopInicio()
+            {
+                if (top == null) return null;
+
+                string valorEliminado = top.Dato;
+                top = top.Sig;
+                Count--;
+                return valorEliminado;
+            }
+            public void PushFinal(string valor)
+            {
+                if (top == null)
+                {
+                    PushFinal(valor);
+                }
+                else
+                {
+                    Nodo nuevoNodo = new Nodo(valor);
+                    Nodo actual = top;
+
+                    while (actual.Sig != null)
+                    {
+                        actual = actual.Sig;
+                    }
+
+                    actual.Sig = nuevoNodo;
+                    Count++;
+                }
+            }
+            public string PopFinal()
+            {
+                if (top == null) return null;
+
+                if (top.Sig == null)
+                {
+                    return PopFinal();
+                }
+                Nodo actual = top;
+                while (actual.Sig.Sig != null)
+                {
+                    actual = actual.Sig;
+                }
+                string valorEliminado = actual.Sig.Dato;
+                actual.Sig = null;
+                Count--;
+                return valorEliminado;
+            }
+            public System.Collections.Generic.List<string> GetElements()
+            {
+                var nodos = new System.Collections.Generic.List<string>();
+                Nodo actual = top;
+
+                while (actual != null)
+                {
+                    nodos.Add(actual.Dato);
+                    actual = actual.Sig;
+                }
+                return nodos;
+            }
+            public bool Buscar(string valor)
+            {
+                Nodo actual = top;
+
+                while (actual != null)
+                {
+                    if (actual.Dato == valor)
+                    {
+                        return true;
+                    }
+                    actual = actual.Sig;
+                }
+
+                return false;
+            }
+
+        }
+        public Pilas()
+        {
+            InitializeComponent();
+        }
+
+        public void ListaPila()
+        {
+            lstPila.Items.Clear();
+            if (miPila.Count > 0)
+            {
+                foreach (string pila in miPila.GetElements())
+                {
+                    lstPila.Items.Add(pila);
+                }
+            }
+        }
+        private void BtnRegresar_Click(object sender, EventArgs e)
+        {
+            MenuFCL men = new MenuFCL();
+            men.Show();
+            Hide();
+        }
+
+        private void ComboPosi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            if (ComboPosi.SelectedIndex != 0 && ComboPosi.SelectedIndex != 1)
+            {
+                miPila.PushInicio(TxtNodo.Text);
+            }
+            if (TxtNodo.Text != "")
+            {
+                if (ComboPosi.SelectedIndex == 0)
+                {
+                    miPila.PushInicio(TxtNodo.Text);
+                }
+                else if (ComboPosi.SelectedIndex == 1)
+                {
+                    miPila.PushFinal(TxtNodo.Text);
+                }
+                TxtNodo.Text = "";
+                TxtNodo.Focus();
+                ListaPila();
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            if (ComboPosi.SelectedIndex != 0 && ComboPosi.SelectedIndex != 1)
+            {
+                miPila.PopInicio();
+            }
+            if (miPila.Count > 0)
+            {
+                if (ComboPosi.SelectedIndex == 0) 
+                {
+                    miPila.PopInicio();
+                }
+                else if (ComboPosi.SelectedIndex == 1) 
+                {
+                    miPila.PopFinal();
+                }
+                ListaPila();
+            }
+            else
+            {
+                MessageBox.Show("La pila está vacía");
+                ListaPila();
+            }
+        }
+
+        private void BtnMostrar_Click(object sender, EventArgs e)
+        {
+            if (miPila.Count > 0)
+            {
+                string nodosmos = string.Join(Environment.NewLine, miPila.GetElements());
+                MessageBox.Show($"Datos ingresados: \n{nodosmos}");
+            }
+            else
+            {
+                MessageBox.Show("La pila está vacía.");
+            }
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            if (TxtNodo.Text != "")
+            {
+                bool nodobus = miPila.Buscar(TxtNodo.Text);
+                if (nodobus)
+                {
+                    MessageBox.Show($"Nodo '{TxtNodo.Text}' encontrado");
+                }
+                else
+                {
+                    MessageBox.Show($"Nodo '{TxtNodo.Text}' no encontrado");
+                }
+                TxtNodo.Text = "";
+                TxtNodo.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un valor para buscar");
+            }
+        }
+    }
+}
